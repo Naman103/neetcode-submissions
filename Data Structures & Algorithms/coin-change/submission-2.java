2229c994f591a1ee59dfc7ce0bda0c9a92ef4145
Naman103 <58305863+Naman103@users.noreply.github.com>
2026-06-10 17:18:24 +0530

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.java
@@ -0,0 +1,57 @@
+class Solution {
+    Integer[][] dp;
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        dp = new Integer[amount + 1][n + 1];
+        //Initializationd
+        for (int i = 0; i <= amount; i++)
+            dp[i][0] = -1;
+        
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = 0;
+        }
+
+        return coinChange(coins, amount, n); 
+        // for(int i = 0; i <= amount; i++) {
+        //     for (int j = 0; j <= n; j++) {
+        //         if (dp[i][j] != null)
+        //             System.out.print(dp[i][j] + " ");
+        //         else
+        //             System.out.print("-2 ");
+        //     }
+
+        //     System.out.println();
+        // }
+        //return dp[amount][n]; 
+    }
+
+    public int coinChange(int[] coins, int amount, int coinIndex) {
+        if (amount == 0) {
+            dp[amount][coinIndex] = 0;
+            return 0;
+        }
+
+        if (amount < 0 || coinIndex <= 0) {
+            return -1;
+        }
+
+        if (dp[amount][coinIndex] != null)
+            return dp[amount][coinIndex];
+    
+    
+        int includedResult = coinChange(coins, amount - coins[coinIndex - 1], coinIndex);
+        int excludedResult = coinChange(coins, amount, coinIndex - 1);
+        
+        if (includedResult == -1 && excludedResult == -1) {
+            dp[amount][coinIndex] = -1;
+        } else if (includedResult != -1 && excludedResult != -1) {
+            dp[amount][coinIndex] = Math.min(1 + includedResult, excludedResult);
+        } else if (includedResult == -1) {
+            dp[amount][coinIndex] = excludedResult;
+        } else if (excludedResult == -1) {
+            dp[amount][coinIndex] = 1 + includedResult;
+        }
+        
+        return dp[amount][coinIndex];
+    }
+}
